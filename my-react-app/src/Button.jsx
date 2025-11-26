8ac7ba226bf9767d9762b487873e90ef7ca2274d
LexanderThakur <lexanderthakur3@gmail.com>
2025-11-26 22:47:45 +0530

    ye

---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,8 @@
+function Button() {
+  return (
+    <>
+      <button className={styles.button}>Click me</button>
+    </>
+  );
+}
+export default Button;
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,16 @@
+function Card() {
+  return (
+    <div className="card">
+      <img
+        className="card-image"
+        src="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
+        alt="profile picture"
+        width={200}
+      />
+      <h2>Bro Code</h2>
+      <p>I make , i just do bro</p>
+    </div>
+  );
+}
+
+export default Card;
