a88145b462768cc66df2e831256cbd9ecf88b851
LexanderThakur <lexanderthakur3@gmail.com>
2025-11-27 22:31:12 +0530

    working with click events

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,17 +1,26 @@
 function Button() {
-  const styles = {
-    backgroundColor: "hsl(200, 100%, 50%)",
-    color: "white",
-    padding: "10px 20px",
-    borderRadius: "5px",
-    border: "none",
-    cursor: "pointer",
-  };
+  // const handleClick = () => console.log("ouch");
 
-  return (
-    <>
-      <button style={styles}>Click me</button>
-    </>
-  );
+  const handleClick = (e) => (e.target.textContent = "OUCH!");
+
+  const handleClick2 = (name) => console.log(`${name} fuck u`);
+  return <button onClick={(e) => handleClick(e)}>Click me</button>;
 }
+
+// function Button() {
+//   const styles = {
+//     backgroundColor: "hsl(200, 100%, 50%)",
+//     color: "white",
+//     padding: "10px 20px",
+//     borderRadius: "5px",
+//     border: "none",
+//     cursor: "pointer",
+//   };
+
+//   return (
+//     <>
+//       <button style={styles}>Click me</button>
+//     </>
+//   );
+// }
 export default Button;
