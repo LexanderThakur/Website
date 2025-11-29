5c63cf7c988c98b0fc1061f94f1dd3390894124f
LexanderThakur <lexanderthakur3@gmail.com>
2025-11-30 00:53:00 +0530

    on Change event handler

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,17 +1,52 @@
 import React, { useState } from "react";
 
 function MyComponent() {
-  let [name, setName] = useState("Jhon");
+  const [name, setName] = useState("Guest");
+  const [quantity, setQuantity] = useState();
 
-  const updateName = () => {
-    setName("Spongebob");
-  };
+  const [comment, setComment] = useState("");
+  function handleCommentChange(event) {
+    setComment(event.target.value);
+  }
+
+  function handleQuantityChange(event) {
+    setQuantity(event.target.value);
+  }
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <div>
+      <input type="text" value={name} onChange={handleNameChange} />
       <p>Name: {name}</p>
-      <button onClick={updateName}>Set Name</button>
+
+      <input value={quantity} onChange={handleQuantityChange} type="number" />
+      <p>Quantity : {quantity}</p>
+      <textarea
+        name=""
+        value={comment}
+        onChange={handleCommentChange}
+        id=""
+        placeholder="wasup"
+      ></textarea>
+      <p>Comment : {comment}</p>
     </div>
   );
 }
+
+// function MyComponent() {
+//   let [name, setName] = useState("Jhon");
+
+//   const updateName = () => {
+//     setName("Spongebob");
+//   };
+//   return (
+//     <div>
+//       <p>Name: {name}</p>
+//       <button onClick={updateName}>Set Name</button>
+//     </div>
+//   );
+// }
 
 export default MyComponent;
